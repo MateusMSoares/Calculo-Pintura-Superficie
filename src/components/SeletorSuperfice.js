b87419761fd483130ddca9124aec110b195e61df
Mateus <mateus.marques100@hotmail.com>
2024-07-29 21:56:36 -0300

    Ajuste layout

--- src/components/SeletorSuperfice.js
+++ src/components/SeletorSuperfice.js
@@ -2,32 +2,40 @@ import React from 'react';
 import '../styles/SeletorSuperfice.css';
 
 const SeletorSuperfice = ({ surfaceType, setSurfaceType, resetDimensions, multiplicidade, setMultiplicidade }) => {
-
     return (
-        <div className="form-group">
-            <div className="input-group">
-                <label>Tipo Superfície:</label>
-                <select
-                    value={surfaceType}
-                    onChange={(e) => {
-                        setSurfaceType(e.target.value);
-                        resetDimensions();
-                    }}
-                >
-                    <option value="">Selecione</option>
-                    <option value="cone">Cone</option>
-                    <option value="circulo">Círculo</option>
-                    <option value="quadrado">Quadrado</option>
-                    <option value="retangulo">Retângulo</option>
-                </select>
-                <label>Fator de Multiplicidade:</label>
-                <input
-                    type="number"
-                    value={multiplicidade}
-                    onChange={(e) => setMultiplicidade(e.target.value)}
-                    min="1"
-                    style={{ marginLeft: '10px' }}
-                />
+        <div className="form-container">
+            <div className="form-row">
+                <div className="form-column">
+                    <label>Tipo Superfície:</label>
+                </div>
+                <div className="form-column">
+                    <label>Fator de Multiplicidade:</label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div className="form-column">
+                    <select
+                        value={surfaceType}
+                        onChange={(e) => {
+                            setSurfaceType(e.target.value);
+                            resetDimensions();
+                        }}
+                    >
+                        <option value="">Selecione</option>
+                        <option value="cone">Cone</option>
+                        <option value="circulo">Círculo</option>
+                        <option value="quadrado">Quadrado</option>
+                        <option value="retangulo">Retângulo</option>
+                    </select>
+                </div>
+                <div className="form-column">
+                    <input
+                        type="number"
+                        value={multiplicidade}
+                        onChange={(e) => setMultiplicidade(parseFloat(e.target.value))}
+                        min="1"
+                    />
+                </div>
             </div>
         </div>
     );
